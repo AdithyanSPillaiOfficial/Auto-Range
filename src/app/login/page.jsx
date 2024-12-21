575ba18ca80be7a00b4f49b111825c48794669b6
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2024-12-21 21:03:24 +0530

    Added Login Page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../../../widgets/login/Login'
+
+function page() {
+  return (
+    <div className='flex justify-center items-center'>
+        <Login />
+    </div>
+  )
+}
+
+export default page
